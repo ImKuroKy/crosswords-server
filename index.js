ec05ce8480104aa9891e18612df0788385d79520
ImKuroKy <spiritofthesovietunion@gmail.com>
2024-12-04 12:32:27 +0400

    feat(auth): Добавление авторизации и регистрации
    - Добавлена модель `user` для запросов в БД.
    - Добавлены валидаторы `authValidators`.
    - Добавлен контроллер `authController`.
    - Добавлен роутер `authRoutes`.
    - Обновлен `index`: подключен роутер на роут `/auth`.
    - Добавлено подключение к БД в `exports`.
    - Добавлен `.gitignore`.
    - Обновлены зависимости в `package.json`.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4400;
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log("Something went wrong: ", err);
+  }
+  console.log("Server is running...");
+  console.log(`Port --> ${port} \n`);
+});
